58f2e7e1ea6e57426bbd39afffb047c9c8391c5b
Henk van der Kuur <info@vanderkuur.com>
2020-09-28 16:29:09 +0200

    Spaarbedrag 1.0

--- src/Spaarbedrag.java
+++ src/Spaarbedrag.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Spaarbedrag {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.print("Wat is uw spaarbedrag op dit moment? ");
+        var s = new Scanner(System.in);
+        var startBedrag = s.nextFloat();
+        System.out.print("Wat is de huidige rente (in %)? ");
+        var rente = s.nextFloat();
+        System.out.print("Hoeveel jaar rente wilt u berekenen? ");
+        var jaren = s.nextInt();
+
+        System.out.println("Met een rente van "+rente+" % per jaar is uw spaargeld na "+jaren+" jaar:");
+        var i = 1;
+        while (i <= jaren){
+            startBedrag = startBedrag*(1+(rente/100));
+            System.out.format("Jaar "+i+": € "+"%.2f%n", +startBedrag);
+            i++;
+        }
+
+    }
 }
